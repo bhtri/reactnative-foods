20ee620ebc970a29608d31c5d954c0e19de3b6bc
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-07-10 19:55:39 +0900

    ⚡️ Navigation

--- App.js
+++ App.js
@@ -1,86 +1,12 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import { COLORS } from './contains';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SeenScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Seen!</Text>
-    </View>
-  );
-}
-
-function FavoriteScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Favorite!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
+import MyTabs from './navigation';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-      initialRouteName='Home'
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            switch (route.name) {
-              case 'Home':
-                iconName = focused
-                  ? 'ios-home'
-                  : 'ios-home-outline';
-                break;
-              case 'Seen':
-                iconName = focused
-                  ? 'ios-timer'
-                  : 'ios-timer-outline';
-                break;
-              case 'Favorite':
-                iconName = focused
-                  ? 'ios-heart'
-                  : 'ios-heart-outline';
-                break;
-              default:
-                break;
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: COLORS.second,
-          tabBarInactiveTintColor: COLORS.second,
-          tabBarStyle: {
-            backgroundColor: COLORS.primary,
-            height: 65,
-            paddingBottom: 10,
-          },
-          tabBarLabelStyle: {
-            fontSize: 13,
-          }
-        })}
-      >
-
-        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Trang chu' }} />
-        <Tab.Screen name="Seen" component={SeenScreen} options={{ title: 'Da xem' }} />
-        <Tab.Screen name="Favorite" component={FavoriteScreen} options={{ title: 'Yeu thich' }} />
-      </Tab.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
---
+++ navigation/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { COLORS } from '../contains';
+import HomeStackScreen from './HomeNavigation';
+import SeenStackScreen from './SeenNavigation';
+import FavoriteScreen from './FavoriteNavigation';
+
+const Tab = createBottomTabNavigator();
+
+export default MyTabs = () => {
+    return (
+
+        <Tab.Navigator
+            initialRouteName='Home'
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    switch (route.name) {
+                        case 'Home':
+                            iconName = focused
+                                ? 'ios-home'
+                                : 'ios-home-outline';
+                            break;
+                        case 'Seen':
+                            iconName = focused
+                                ? 'ios-timer'
+                                : 'ios-timer-outline';
+                            break;
+                        case 'Favorite':
+                            iconName = focused
+                                ? 'ios-heart'
+                                : 'ios-heart-outline';
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: COLORS.second,
+                tabBarInactiveTintColor: COLORS.second,
+                tabBarStyle: {
+                    backgroundColor: COLORS.primary,
+                    height: 65,
+                    paddingBottom: 10,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 13,
+                }
+            })}
+        >
+
+            <Tab.Screen name="Home" component={HomeStackScreen} options={{ title: 'Trang chu' }} />
+            <Tab.Screen name="Seen" component={SeenStackScreen} options={{ title: 'Da xem' }} />
+            <Tab.Screen name="Favorite" component={FavoriteScreen} options={{ title: 'Yeu thich' }} />
+        </Tab.Navigator>
+    );
+}
