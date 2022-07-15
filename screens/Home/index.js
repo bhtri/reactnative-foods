aac910a3146b619c944e27fb246344fad6308015
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-07-15 23:58:08 +0900

    ⚡️ Flatlist

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,8 +1,9 @@
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
 import React from 'react'
 
 import styles from './style'
 import { Category } from './../../components';
+import { CATEGORIES } from '../../data';
 
 export default function HomeScreen({ navigation }) {
 
@@ -12,11 +13,12 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Category />
-      <Category />
-      <Category />
-      <Category />
-      <Category />
+      <FlatList
+        data={CATEGORIES}
+        numColumns={2}
+        renderItem={() => <Category />}
+        keyExtractor={item => item.id}
+      />
     </View>
   )
 }
