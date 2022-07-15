9db20b692973f82405d9cc81e37b17afa994c325
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-07-16 01:28:46 +0900

    ⚡️ Redux Toolkit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
 
 import MyTabs from './navigation';
 
+import store from './store';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </Provider>
   );
 }
